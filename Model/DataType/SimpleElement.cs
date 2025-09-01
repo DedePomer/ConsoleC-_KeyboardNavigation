618104b09f2a99e1b814cc3f2da1230640f9375b
t343s <t343s@PC-01>
2025-09-01 23:37:38 +0300

    +

--- Model/DataType/SimpleElement.cs
+++ Model/DataType/SimpleElement.cs
@@ -9,7 +9,7 @@
 
         public event Action<object?> ExecuteElement;
         public void Execute(object? obj) => ExecuteElement?.Invoke(obj);
-        public void DisplayElement()
+        public void DisplayElement() /*наверно не должен сдесь быть*/
         {
             Console.WriteLine(Text);
         }
